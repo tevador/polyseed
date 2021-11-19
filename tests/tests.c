a4c3b8392858fdd666875cdb835051966ac6bc5e
tevador <tevador@gmail.com>
2021-11-19 09:12:14 +0100

    fix code indentation

--- tests/tests.c
+++ tests/tests.c
@@ -82,8 +82,8 @@ static const polyseed_lang* g_lang_es;
 #define RUN_MULT(x) run_multitest(#x, &x)
 
 static void run_test(const char* name, test_func* func) {
-	printf("[%2i] %-40s ... ", ++g_test_no, name);
-	printf(func() ? "PASSED\n" : "SKIPPED\n");
+    printf("[%2i] %-40s ... ", ++g_test_no, name);
+    printf(func() ? "PASSED\n" : "SKIPPED\n");
 }
 
 static void run_multitest(const char* name, multitest_func* func) {
@@ -100,54 +100,54 @@ static void subtest_end(bool passed) {
 }
 
 static void output_hex(const char* data, size_t length) {
-	for (unsigned i = 0; i < length; ++i)
-		printf("%02x", data[i] & 0xff);
+    for (unsigned i = 0; i < length; ++i)
+        printf("%02x", data[i] & 0xff);
 }
 
 static inline char parse_nibble(char hex) {
-	hex &= ~0x20;
-	return (hex & 0x40) ? hex - ('A' - 10) : hex & 0xf;
+    hex &= ~0x20;
+    return (hex & 0x40) ? hex - ('A' - 10) : hex & 0xf;
 }
 
 static inline bool equals_hex(const void* bin, const char* hex) {
-	size_t hexlen = strlen(hex);
-	assert((hexlen % 2) == 0);
-	const char* pbin = bin;
-	for (int i = 0; i < hexlen; i += 2) {
-		char nibble1 = parse_nibble(*hex++);
-		char nibble2 = parse_nibble(*hex++);
-		uint8_t val = (uint8_t)*pbin++;
-		uint8_t ref = (uint8_t)nibble1 << 4 | nibble2;
-		if (val != ref) {
-			return false;
-		}
-	}
-	return true;
+    size_t hexlen = strlen(hex);
+    assert((hexlen % 2) == 0);
+    const char* pbin = bin;
+    for (int i = 0; i < hexlen; i += 2) {
+        char nibble1 = parse_nibble(*hex++);
+        char nibble2 = parse_nibble(*hex++);
+        uint8_t val = (uint8_t)*pbin++;
+        uint8_t ref = (uint8_t)nibble1 << 4 | nibble2;
+        if (val != ref) {
+            return false;
+        }
+    }
+    return true;
 }
 
 static const polyseed_lang* get_lang(const char* name) {
-	int count = polyseed_get_num_langs();
-	for (int i = 0; i < count; ++i) {
-		const polyseed_lang* lang = polyseed_get_lang(i);
-		if (0 == strcmp(polyseed_get_lang_name_en(lang), name)) {
-			return lang;
-		}
-	}
-	return NULL;
+    int count = polyseed_get_num_langs();
+    for (int i = 0; i < count; ++i) {
+        const polyseed_lang* lang = polyseed_get_lang(i);
+        if (0 == strcmp(polyseed_get_lang_name_en(lang), name)) {
+            return lang;
+        }
+    }
+    return NULL;
 }
 
 #define TEST_KEYLEN 32
 
 static const char rand_bytes1[] = {
-	0xdd, 0x76, 0xe7, 0x35, 0x9a, 0x0d, 0xed, 0x37,
-	0xcd, 0x0f, 0xf0, 0xf3, 0xc8, 0x29, 0xa5, 0xae,
-	0x01, 0x67, 0xf3, 0xdb, 0x0f, 0x6d, 0x1b, 0x8f,
-	0xad, 0x7d, 0x52, 0x81, 0x1c, 0x14, 0x00, 0xb8,
+    0xdd, 0x76, 0xe7, 0x35, 0x9a, 0x0d, 0xed, 0x37,
+    0xcd, 0x0f, 0xf0, 0xf3, 0xc8, 0x29, 0xa5, 0xae,
+    0x01, 0x67, 0xf3, 0xdb, 0x0f, 0x6d, 0x1b, 0x8f,
+    0xad, 0x7d, 0x52, 0x81, 0x1c, 0x14, 0x00, 0xb8,
 };
 
 static void gen_rand_bytes1(void* result, size_t n) {
-	assert(n == sizeof(rand_bytes1));
-	memcpy(result, rand_bytes1, sizeof(rand_bytes1));
+    assert(n == sizeof(rand_bytes1));
+    memcpy(result, rand_bytes1, sizeof(rand_bytes1));
 }
 
 static const char rand_bytes2[] = {
@@ -158,29 +158,29 @@ static const char rand_bytes2[] = {
 };
 
 static void gen_rand_bytes2(void* result, size_t n) {
-	assert(n == sizeof(rand_bytes2));
-	memcpy(result, rand_bytes2, sizeof(rand_bytes2));
+    assert(n == sizeof(rand_bytes2));
+    memcpy(result, rand_bytes2, sizeof(rand_bytes2));
 }
 
 static const char rand_bytes3[] = {
-	0x67, 0xb9, 0x36, 0xdf, 0xa4, 0xda, 0x6a, 0xe8,
-	0xd3, 0xb3, 0xcd, 0xb3, 0xb9, 0x37, 0xf4, 0x02,
-	0x7b, 0x0e, 0x3b, 0x15, 0xd3, 0x9b, 0xea, 0xef,
-	0x25, 0x3e, 0x21, 0x15, 0xae, 0xd0, 0x45, 0xdc,
+    0x67, 0xb9, 0x36, 0xdf, 0xa4, 0xda, 0x6a, 0xe8,
+    0xd3, 0xb3, 0xcd, 0xb3, 0xb9, 0x37, 0xf4, 0x02,
+    0x7b, 0x0e, 0x3b, 0x15, 0xd3, 0x9b, 0xea, 0xef,
+    0x25, 0x3e, 0x21, 0x15, 0xae, 0xd0, 0x45, 0xdc,
 };
 
 static void gen_rand_bytes3(void* result, size_t n) {
-	assert(n == sizeof(rand_bytes3));
-	memcpy(result, rand_bytes3, sizeof(rand_bytes3));
+    assert(n == sizeof(rand_bytes3));
+    memcpy(result, rand_bytes3, sizeof(rand_bytes3));
 }
 
 static void pbkdf2_dummy1(const uint8_t* pw, size_t pwlen,
-	const uint8_t* salt, size_t saltlen, uint64_t iterations,
-	uint8_t* key, size_t keylen) {
-	assert(equals_hex(pw,
-		"dd76e7359a0ded37cd0ff0f3c829a5ae01673300000000000000000000000000"));
-	assert(equals_hex(salt,
-		"504f4c5953454544207631000000000001000000000000000000000000000000"));
+    const uint8_t* salt, size_t saltlen, uint64_t iterations,
+    uint8_t* key, size_t keylen) {
+    assert(equals_hex(pw,
+        "dd76e7359a0ded37cd0ff0f3c829a5ae01673300000000000000000000000000"));
+    assert(equals_hex(salt,
+        "504f4c5953454544207631000000000001000000000000000000000000000000"));
 }
 
 static void pbkdf2_dummy2(const uint8_t* pw, size_t pwlen,
@@ -202,118 +202,118 @@ static void pbkdf2_dummy3(const uint8_t* pw, size_t pwlen,
 }
 
 static void u8_nfc_donothing(const char* str, polyseed_str norm) {
-	/* do not compose */
+    /* do not compose */
     strncpy(norm, str, POLYSEED_STR_SIZE);
 }
 
 static void u8_nfkd_spaces(const char* str, polyseed_str norm) {
-	/* normalize only ideographic spaces to allow Japanese phrases to roundtrip */
-	int i = 0;
-	for (; i < POLYSEED_STR_SIZE && *str != '\0'; ++i) {
-		if (str[0] == '\xe3' && str[1] == '\x80' && str[2] == '\x80') {
-			norm[i] = ' ';
+    /* normalize only ideographic spaces to allow Japanese phrases to roundtrip */
+    int i = 0;
+    for (; i < POLYSEED_STR_SIZE && *str != '\0'; ++i) {
+        if (str[0] == '\xe3' && str[1] == '\x80' && str[2] == '\x80') {
+            norm[i] = ' ';
             str += 3;
-		}
-		else {
-			norm[i] = *str;
+        }
+        else {
+            norm[i] = *str;
             ++str;
-		}
-	}
-	if (i < POLYSEED_STR_SIZE) {
-		norm[i] = '\0';
-	}
+        }
+    }
+    if (i < POLYSEED_STR_SIZE) {
+        norm[i] = '\0';
+    }
 }
 
 static time_t time1(time_t* t) {
-	return SEED_TIME1;
+    return SEED_TIME1;
 }
 
 static time_t time2(time_t* t) {
-	return SEED_TIME2;
+    return SEED_TIME2;
 }
 
 static time_t time3(time_t* t) {
-	return SEED_TIME3;
+    return SEED_TIME3;
 }
 
 static void check_key(polyseed_data* data, polyseed_coin coin) {
-	char key[TEST_KEYLEN];
-	polyseed_keygen(data, coin, TEST_KEYLEN, key);
+    char key[TEST_KEYLEN];
+    polyseed_keygen(data, coin, TEST_KEYLEN, key);
 }
 
 static bool test_inject1(void) {
-	const polyseed_dependency deps = {
-		.rand = &gen_rand_bytes1,
-		.pbkdf2_sha256 = &pbkdf2_dummy1,
-		.u8_nfc = &u8_nfc_donothing,
-		.u8_nfkd = &u8_nfkd_spaces,
-		.time = &time1,
-	};
-	polyseed_inject(&deps);
-	return true;
+    const polyseed_dependency deps = {
+        .rand = &gen_rand_bytes1,
+        .pbkdf2_sha256 = &pbkdf2_dummy1,
+        .u8_nfc = &u8_nfc_donothing,
+        .u8_nfkd = &u8_nfkd_spaces,
+        .time = &time1,
+    };
+    polyseed_inject(&deps);
+    return true;
 }
 
 static bool test_num_langs(void) {
     g_num_langs = polyseed_get_num_langs();
-	assert(g_num_langs > 0);
-	return true;
+    assert(g_num_langs > 0);
+    return true;
 }
 
 static bool test_get_lang(void) {
-	for (int i = 0; i < g_num_langs; ++i) {
-		const polyseed_lang* lang = polyseed_get_lang(i);
-		assert(lang != NULL);
-	}
-	return true;
+    for (int i = 0; i < g_num_langs; ++i) {
+        const polyseed_lang* lang = polyseed_get_lang(i);
+        assert(lang != NULL);
+    }
+    return true;
 }
 
 static void test_get_lang_name(void) {
-	for (int i = 0; i < g_num_langs; ++i) {
-		const polyseed_lang* lang = polyseed_get_lang(i);
+    for (int i = 0; i < g_num_langs; ++i) {
+        const polyseed_lang* lang = polyseed_get_lang(i);
         subtest_begin(polyseed_get_lang_name_en(lang));
-		assert(polyseed_get_lang_name(lang) != NULL);
-		assert(polyseed_get_lang_name_en(lang) != NULL);
+        assert(polyseed_get_lang_name(lang) != NULL);
+        assert(polyseed_get_lang_name_en(lang) != NULL);
         subtest_end(true);
-	}
+    }
 }
 
 static bool test_create1(void) {
-	polyseed_create(&g_seed1);
-	return true;
+    polyseed_create(&g_seed1);
+    return true;
 }
 
 static bool test_birthday1(void) {
-	time_t birthday = polyseed_get_birthday(&g_seed1);
-	assert(birthday <= SEED_TIME1);
-	return true;
+    time_t birthday = polyseed_get_birthday(&g_seed1);
+    assert(birthday <= SEED_TIME1);
+    return true;
 }
 
 static bool test_keygen1(void) {
-	char key[TEST_KEYLEN];
-	polyseed_keygen(&g_seed1, POLYSEED_MONERO, TEST_KEYLEN, key);
-	return true;
+    char key[TEST_KEYLEN];
+    polyseed_keygen(&g_seed1, POLYSEED_MONERO, TEST_KEYLEN, key);
+    return true;
 }
 
 static bool test_encode_en(void) {
-	g_lang_en = get_lang("English");
-	if (g_lang_en == NULL) {
-		return false;
-	}
-	polyseed_encode(&g_seed1, g_lang_en, POLYSEED_MONERO, g_phrase_out);
-	assert(0 == strcmp(g_phrase_out, g_phrase_en1));
-	return true;
+    g_lang_en = get_lang("English");
+    if (g_lang_en == NULL) {
+        return false;
+    }
+    polyseed_encode(&g_seed1, g_lang_en, POLYSEED_MONERO, g_phrase_out);
+    assert(0 == strcmp(g_phrase_out, g_phrase_en1));
+    return true;
 }
 
 static bool test_decode_en(void) {
-	if (g_lang_en == NULL) {
-		return false;
-	}
-	const polyseed_lang* lang;
-	polyseed_data seed;
-	polyseed_status res = polyseed_decode(g_phrase_en1, POLYSEED_MONERO, &lang, &seed);
-	assert(res == POLYSEED_OK);
+    if (g_lang_en == NULL) {
+        return false;
+    }
+    const polyseed_lang* lang;
+    polyseed_data seed;
+    polyseed_status res = polyseed_decode(g_phrase_en1, POLYSEED_MONERO, &lang, &seed);
+    assert(res == POLYSEED_OK);
     assert(lang == g_lang_en);
-	check_key(&seed, POLYSEED_MONERO);
+    check_key(&seed, POLYSEED_MONERO);
     return true;
 }
 
@@ -554,15 +554,15 @@ static bool test_decode_garbage2(void) {
 }
 
 int main() {
-	RUN_TEST(test_inject1);
-	RUN_TEST(test_num_langs);
-	RUN_TEST(test_get_lang);
-	RUN_MULT(test_get_lang_name);
-	RUN_TEST(test_create1);
-	RUN_TEST(test_birthday1);
-	RUN_TEST(test_keygen1);
-	RUN_TEST(test_encode_en);
-	RUN_TEST(test_decode_en);
+    RUN_TEST(test_inject1);
+    RUN_TEST(test_num_langs);
+    RUN_TEST(test_get_lang);
+    RUN_MULT(test_get_lang_name);
+    RUN_TEST(test_create1);
+    RUN_TEST(test_birthday1);
+    RUN_TEST(test_keygen1);
+    RUN_TEST(test_encode_en);
+    RUN_TEST(test_decode_en);
     RUN_TEST(test_decode_en_prefix);
     RUN_TEST(test_decode_en_suffix1);
     RUN_TEST(test_decode_en_suffix2);
@@ -587,6 +587,6 @@ int main() {
     RUN_TEST(test_decode_garbage1);
     RUN_TEST(test_decode_garbage2);
 
-	printf("\nAll tests were successful\n");
-	return 0;
+    printf("\nAll tests were successful\n");
+    return 0;
 }
